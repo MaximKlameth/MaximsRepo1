aa00e450e4fbd2fd745f76196eb9a3b5c5242f4c
MaximKlameth <maxim.klameth@gmail.com>
2020-01-24 11:56:40 +0100

    Codeausschnitte entfernt

--- src/ButtonBarView.java
+++ src/ButtonBarView.java
@@ -33,9 +33,7 @@ public class ButtonBarView extends JPanel {
 		schwerButton.addActionListener(snakeController.getSchwierigkeitErhoehenListener());
 		add(schwerButton);
 		
-		spielBeendenButton = new JButton("Spiel abbrechen");
-		spielBeendenButton.addActionListener(snakeController.getExitGameListener());
-		add(spielBeendenButton);	
+			
 
 		
 		
